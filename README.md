ed1285f554474b81a4c4cc5477e213e65008f094
Thomas Shepherd <tshep@sertiscorp.com>
2019-04-29 15:26:54 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Game
+  
+game
+  
+## Dependencies
+
+In this example, there are only 4 dependencies!
+
+- `ganache-core` — Allows us to spawn a "test blockchain".
+- `jest` — A testing framework.
+- `solc` — Compiles our Solidity contract.
+- `web3` — Allows us to interact with our contract and do other Ethereum things.
+
---
+++ test.js
@@ -0,0 +1,53 @@
+/**
+ * @jest-environment node
+ */
+const Ganache = require("ganache-core");
+const Web3 = require("web3");
+const compile = require("./compile");
+
+describe("test stuff", () => {
+  let contractInstance;
+  let accounts;
+  let provider;
+  let web3;
+  beforeAll(async () => {
+    // 1. Compile contract artifact
+    const { SimpleStorage } = await compile("SimpleStorage.sol");
+
+    // 2. Spawn Ganache test blockchain
+    provider = Ganache.provider();
+    web3 = new Web3(provider);
+    accounts = await web3.eth.getAccounts();
+
+    // 3. Create initial contract instance
+    const instance = new web3.eth.Contract(SimpleStorage.abi);
+
+    // 4. Deploy contract and get new deployed instance
+    const deployedInstance = await instance
+      .deploy({ data: SimpleStorage.evm.bytecode.object })
+      .send({ from: accounts[0], gas: 150000 });
+
+    // 5. Assign deployed contract instance to variable
+    contractInstance = deployedInstance;
+  });
+
+  afterAll(async () => {
+    // clean up provider
+    provider.stop();
+  });
+
+  it("should test contract", async () => {
+    // get old value
+    const oldVal = await contractInstance.methods.get().call();
+
+    // set new value
+    await contractInstance.methods.set(5).send({ from: accounts[0] });
+
+    // get new value
+    const newVal = await contractInstance.methods.get().call();
+
+    // assert our expectations
+    expect(oldVal).toBe("0");
+    expect(newVal).toBe("5");
+  });
+});
