dcda7dbdb6beb7c67e81adb4f3a588465ed078a5
Fei <thomasshepherd2002@gmail.com>
2019-05-01 20:28:43 +0700

    Update earningDivider contract

--- earningDivider.sol
+++ earningDivider.sol
@@ -7,6 +7,7 @@ contract EarningsDivider{
     bool exist;
   }
   mapping (address => Holder) holders;
+  address[] addressList
 
   constructor() public {
     holders[msg.sender] = Holder({
@@ -18,11 +19,11 @@ contract EarningsDivider{
 
   function giveShares(address receiver, uint8 shares) external {
     require(holders[msg.sender].shares > 0);
+    
     uint8 memory currentShares = holders[msg.sender].shares;
     require(shares < currentShares && shares > 0);
 
     holders[msg.sender].shares -= shares;
-
     if(holders[receiver].exist) {
       holders[receiver].shares += shares;
     }else{
@@ -30,19 +31,25 @@ contract EarningsDivider{
         balance: 0,
         shares: shares
       });
+      addressList.push(receiver);
     }
   }
 
-  function addNewHolder(address receiver, uint8 shares) internal {
-
-  }
-
   function withdraw() external {
+    require(holders[msg.sender].balance > 0);
+    
+    address(this).send(bal);   
+    holders[msg.sender].balance = 0;
   }
 
-  function getDistribution() external {
+  function getDistribution() external view {
+    return holders;
   }
 
   function() payable external {
+    uint memory dividends = (msg.value + address(this).balance) / addressList.length;
+    for(uint i = 0; i < addressList.length; i++){
+      holders[addressList[i]].balance += dividends;
+    }
   }
 }
